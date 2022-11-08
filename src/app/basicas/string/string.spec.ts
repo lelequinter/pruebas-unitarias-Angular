93713695c6901263816c3a37468699b97f7944d4
Leyder Quintero <lele.quinter@gmail.com>
2022-11-08 11:01:13 -0500

    Pruebas unitarias

---
+++ src/app/basicas/string/string.spec.ts
@@ -0,0 +1,6 @@
+// Describe sirve para agrupar pruebas
+// describe( 'Pruebas de strings' );
+
+// Los it son pruebas en sí
+it( 'Debe de regresar una string' );
+it( 'Debe de contener un nombre' );
