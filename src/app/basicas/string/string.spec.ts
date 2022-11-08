d081a2df08a05ae5efa6f7d49af1e5e4b34e7b65
Leyder Quintero <lele.quinter@gmail.com>
2022-11-08 15:16:23 -0500

    Probando Strings

--- src/app/basicas/string/string.spec.ts
+++ src/app/basicas/string/string.spec.ts
@@ -2,5 +2,18 @@
 // describe( 'Pruebas de strings' );
 
 // Los it son pruebas en sí
-it( 'Debe de regresar una string' );
-it( 'Debe de contener un nombre' );
+// it( 'Debe de regresar una string' );
+
+import { mensaje } from "./string";
+
+describe('Pruebas de strings', () => {
+
+  it( 'Debe de regresar un string', () => {
+
+    const resp =  mensaje('Fernando');
+
+    expect( typeof resp ).toBe('string');
+
+  });
+
+});
