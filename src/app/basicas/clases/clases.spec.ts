62615992657985c9e0ae3f0449566bb1f11fde9c
Leyder Quintero <lele.quinter@gmail.com>
2022-11-08 18:22:23 -0500

    BeforeEach - BeforeAll - AfterAll - After Each

--- src/app/basicas/clases/clases.spec.ts
+++ src/app/basicas/clases/clases.spec.ts
@@ -2,8 +2,15 @@ import { Jugador } from "./clases";
 
 describe('Pruebas en clases', () => {
 
-  const jugador = new Jugador();
+  let jugador = new Jugador();
   // reset de la propiedad para cada prueba
+
+  beforeEach( () => {
+    // console.log('beforeEach');
+    // jugador.hp = 100;
+    jugador = new Jugador();
+  });
+
 
   it('Debe de retornar 80 de hp, si recibe 20 de danio', () => {
 
@@ -17,6 +24,13 @@ describe('Pruebas en clases', () => {
     const resp = jugador.recibeDanio(50);
 
     expect(resp).toBe(50);
+  });
+
+  it('Debe de retornar 0 de hp, si recibe mas de 100 de danio', () => {
+
+    const resp = jugador.recibeDanio(150);
+
+    expect(resp).toBe(0);
   });
 
 });
