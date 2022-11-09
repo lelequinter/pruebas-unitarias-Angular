38dc6c7ba6d6ec5585b91e4f338fe41a8f351a4a
Leyder Quintero <lele.quinter@gmail.com>
2022-11-09 12:03:13 -0500

    Validaciones de formularios

--- src/app/intermedio/formularios/formulario.spec.ts
+++ src/app/intermedio/formularios/formulario.spec.ts
@@ -9,9 +9,24 @@ describe('Formularios', () => {
 
   it('Debe de crear un formulario con dos campos, email y password', () => {
 
-    expect( componente.form.contains('email') ).toBeTruthy();
-    expect( componente.form.contains('password') ).toBeTruthy();
+    expect(componente.form.contains('email')).toBeTruthy();
+    expect(componente.form.contains('password')).toBeTruthy();
+  });
 
+  it('El email debe de ser obligatorio', () => {
+    const control = componente.form.get('email');
+
+    control?.setValue('');
+
+    expect(control?.valid).toBeFalsy();
+  });
+
+  it('El email debe de ser un correo valido', () => {
+    const control = componente.form.get('email');
+
+    control?.setValue('lele@mail.com');
+
+    expect(control?.valid).toBe(true);
   });
 
 
